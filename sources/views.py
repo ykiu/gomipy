b46aea5b5af0f6d81ba3d9a70f2ba69b65c7fb70
M. Toyokura <32252611+MToyokura@users.noreply.github.com>
2017-11-15 22:47:50 +0900

    2列まで

--- sources/views.py
+++ sources/views.py
@@ -13,31 +13,39 @@ class MainWindow(QWidget):
         '''
         super().__init__()
         self.setWindowTitle('電卓的な練習の模倣')
-        self.setGeometry(300, 200, 400, 300)
 
         outer_layout = QHBoxLayout()
+
+        #ここから左カラムの作成
         left_container = QFrame()
-        left = QGridLayout(left_container)
+        left = QVBoxLayout(left_container)
 
         #ラベル作成
         lbl11 = QLabel("商品番号",self)
         lbl12 = QLabel("値引き後の値段\r\n（初期価格は空白）", self)
-        
+
+        #入力バー作成
+        self.txtbox11 = QLineEdit(self)
+        self.txtbox12 = QLineEdit(self)
+
         #ボタン作成
         add_to_cart_button = QPushButton("カートに追加",self)
-        add_to_cart_button.clicked.connect(self.on_click)
+        add_to_cart_button.clicked.connect(self.pass_on_click)
+
+        #leftのwidget配置
+        left.addWidget(lbl11)
+        left.addWidget(self.txtbox11)
+        left.addStretch(1)
+        left.addWidget(lbl12)
+        left.addWidget(self.txtbox12)
+        left.addStretch(2)
+        left.addWidget(add_to_cart_button)
+        left.addStretch(10)
 
 
-
-
-
-
+        #ここから右カラムの作成
         right_container = QFrame()
         right = QGridLayout(right_container)
-
-        #OKボタン作成
-        OKbutton = QPushButton("O K",self)
-        OKbutton.clicked.connect(self.on_click)
 
         #ラベル作成
         lbl1 = QLabel("値段",self)
@@ -45,21 +53,26 @@ class MainWindow(QWidget):
         lbl3 = QLabel("おつり", self)
         self.lbl4 = QLabel("",self)
 
-
-        right.addWidget(lbl1,0,0)
-        right.addWidget(lbl2,1,0)
-        right.addWidget(lbl3,2,0)
-        right.addWidget(self.lbl4,2,1)
-
+        # 入力バー作成
         self.txtbox1 = QLineEdit(self)
         self.txtbox2 = QLineEdit(self)
 
+        # OKボタン作成
+        OKbutton = QPushButton("OK", self)
+        OKbutton.clicked.connect(self.calc_on_click)
+
+        #rightのwidget配置
+        right.addWidget(lbl1, 0, 0)
+        right.addWidget(lbl2, 1, 0)
+        right.addWidget(lbl3, 2, 0)
+        right.addWidget(self.lbl4, 2, 1)
         right.addWidget(self.txtbox1,0,1)
         right.addWidget(self.txtbox2,1,1)
-
         right.addWidget(OKbutton,2,2)
 
-        
+
+        #大枠の配置
+        outer_layout.addWidget(left_container)
         outer_layout.addWidget(right_container)
 
         self.setLayout(outer_layout)
@@ -67,7 +80,7 @@ class MainWindow(QWidget):
 
         self.show()
 
-    def on_click(self):
+    def calc_on_click(self):
        # self.a = Do_not_touch_me()
         p = int(self.txtbox1.text())
         m = int(self.txtbox2.text())
@@ -76,6 +89,8 @@ class MainWindow(QWidget):
 
         print(p,m,change)
 
+    def pass_on_click(self):
+        pass
         
 def Calc(price, money):
     return money - price
